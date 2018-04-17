bf11527063384903c138706022d81a350531e804
Rajesh Vijayan <A-8076@ibsplc.com>
2018-04-17 17:42:57 +0530

    Hero component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HerosComponent } from './heros/heros.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heros';
 }
